374f24bb1536eb55352cf8ed7200d0f148daee17
GaneshDeshmane <skycode.bots@gmail.com>
2026-09-04 20:25:57 +0530

    schema for db

--- db/db.ts
+++ db/db.ts
@@ -1,6 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Prisma, PrismaClient } from "../generated/prisma/client"
 
+
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 async function main() {
