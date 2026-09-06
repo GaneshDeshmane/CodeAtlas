8ae9062d5d0c5941322c1805bceb72dd0123b680
GaneshDeshmane <skycode.bots@gmail.com>
2026-09-06 23:45:47 +0530

    store embedding add

--- services/embedding.ts
+++ services/embedding.ts
@@ -1,13 +1,21 @@
 import { processRepo } from "./repository"
 import { chuncking } from "./chunker"
 import type{ chunkModel ,repositoryModel,embeddingModel ,filesModel,UserQueryModel } from "../generated/prisma/models"
-import { Prisma } from "../generated/prisma/client"
-
+import type{ embedding , chunk , repository , files , UserQuery } from "../generated/prisma/client"
+import { Prisma, PrismaClient } from "../generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({adapter})
 export const storeRepo = async function (repository : string) {
     const processdata = await processRepo(repository)
-    const content = processdata.content
-    const file = processdata.files
-    const chunksdata = await chuncking(content)
-    
-    
+    //const content = processdata.content
+    const file = processdata.files[0]?.content
+    // const chunksdata = await chuncking(content)
+//    const storedata = await prisma.chunk.createMany({
+//     data :[
+       
+//     ]
+//    })
+console.log(file)
 }
+storeRepo('https://github.com/GaneshDeshmane/llm-orchestrator')
