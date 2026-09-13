7f8cbdcaf6ddd07e9643b48b310663e34c1364fb
GaneshDeshmane <skycode.bots@gmail.com>
2026-09-13 13:51:03 +0530

    storing data

--- services/embedding.ts
+++ services/embedding.ts
@@ -8,15 +8,34 @@ const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({adapter})
 export const storeRepo = async function (repository : string) {
     const processdata = await processRepo(repository)
-    //const content = processdata.content
-    const file = processdata.files[0]?.content
-    // const chunksdata = await chuncking(content)
-//    const storedata = await prisma.chunk.createMany({
-//     data :[
-       
-//     ]
-//    })
-console.log(file)
+    // const file = processdata.files[0]?.content
+    // const size =processdata.files[0]?.size
+    //  const chunksdata = await chuncking(repository)
+    const repositorydata = await prisma.repository.create({
+        data:{
+            name : processdata.metadata.name,
+            path:processdata.path,
+            owner:processdata.owner,
+            url:processdata.repository
+        } 
+    }
+    )
+    const firstcontent=processdata.files[0]?.content
+    if (!firstcontent) {
+        throw new Error('file doesnt there')
+    }
+    const firstpath = processdata.files[0]?.path
+    if (!firstpath) {
+        throw new Error('file doesnt there')
+    }
+    await prisma.files.create({
+        data:{
+            repositoryId:repositorydata.id,
+            fileContent:firstcontent,
+            path:firstpath
+        }
+    })
+
 }
 
 storeRepo("https://github.com/GaneshDeshmane/llm-orchestrator")
--- services/retrieval.ts
+++ services/retrieval.ts
@@ -1,4 +1,9 @@
 import type{respo}from'./types'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Prisma, PrismaClient } from "../generated/prisma/client"
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 const OLLAMA_API = process.env.OLLAMA_API
 export async function Retrieve(text : string) {
     async function generateEmbedding(query:string){
@@ -17,13 +22,14 @@ export async function Retrieve(text : string) {
         }
         const respo = await result.json() as respo
         return respo.embedding
+        
     }
-   
-    const embedding : number[] = await generateEmbedding(text)
+    const embedding = await generateEmbedding(text)
     console.log(embedding)
     const embeddingVector:string = `[${embedding.join(",")}]`
-    
-`
+  
+ async function  storeCode(){
+const resultQuery=await prisma.$queryRaw`
 SELECT
       c."chunkId",
       c."content",
@@ -32,9 +38,14 @@ SELECT
     FROM "embedding" e
     JOIN "chunk" c
       ON c."chunkId" = e."chunkId"
-    WHERE c."filesId" = ${file.id}
+
     ORDER BY e."data" <=> ${embeddingVector}::vector
     LIMIT 3
   `;
+  return resultQuery
+}
+const result=await storeCode()
+console.log(result)
+return result
 }
 Retrieve('hi there')
